4142219f1289e49f9111b81b669fa48b9e018ccd
Abdullah Al Sady <abdullahalsady09@gmail.com>
2023-07-05 21:54:50 +0600

    bubble_sort and selection_sort file modified

--- Sorting Algorithms/Bubble_sort.py
+++ Sorting Algorithms/Bubble_sort.py
@@ -1,11 +1,10 @@
-def bubble_sort(array):
-    length = len(array)
-    for i in range(length):
-        for j in range(length-i-1):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
-    return array
-
 array = [10, 9, 5, 4, 100, 132]
-sorted_array = bubble_sort(array)
-print(sorted_array)
+length = len(array)
+
+for i in range(length):
+    for j in range(length-i-1):
+        if array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1], array[j]
+
+
+print(array)
